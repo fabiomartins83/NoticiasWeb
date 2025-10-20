fca81238c12c0c61c582aaf20e8a863f5f2227c7
Fabio de Almeida Martins <fabio.martins.01@hotmail.com>
2025-10-20 01:14:59 -0300

    correções em index.php

--- index.php
+++ index.php
@@ -18,22 +18,15 @@ function formatarDataExtenso($datetimeStr) {
     $dt = DateTime::createFromFormat('Ymd\THis', $datetimeStr);
     if (!$dt) return "Data inválida";
 
-    // Tentativa de setar locale PT-BR, se disponível
-    $locales = ['pt_BR.utf8', 'pt_BR.UTF-8', 'portuguese'];
-    foreach ($locales as $loc) {
-        if (setlocale(LC_TIME, $loc) !== false) break;
-    }
+    setlocale(LC_TIME, 'pt_BR.utf8', 'pt_BR.UTF-8', 'portuguese');
+    $dataFormatada = utf8_encode(strftime('%A, %d de %B de %Y', $dt->getTimestamp()));
 
-    // Formata a data
-    $dataFormatada = strftime('%A, %d de %B de %Y', $dt->getTimestamp());
-    $dataFormatada = utf8_encode($dataFormatada);
-
-    // Usa mbstring se disponível
+    // Verifica se a função mb_convert_case existe antes de usar
     if (function_exists('mb_convert_case')) {
         $dataFormatada = mb_convert_case($dataFormatada, MB_CASE_TITLE, "UTF-8");
         $dataFormatada = str_replace(["De ", "Feira"], ["de ", "feira"], $dataFormatada);
     } else {
-        // fallback simples se mbstring não estiver habilitada
+        // Caso mbstring não esteja habilitada, usa fallback simples
         $dataFormatada = ucwords($dataFormatada);
     }
 
