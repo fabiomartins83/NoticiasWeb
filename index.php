04e3855ac40ae9d5c82e1c634134d6d47bdfa8cf
Fabio de Almeida Martins <fabio.martins.01@hotmail.com>
2025-10-23 08:26:25 -0300

    corrigindo tamanho/formato dos skeletons

--- index.php
+++ index.php
@@ -454,9 +454,8 @@ document.addEventListener("DOMContentLoaded", async () => {
         for (let i = 0; i < numColunas * 2; i++) {
             const s = document.createElement("div");
             s.classList.add("skeleton");
-            // usa o mesmo cálculo e variáveis dos cards reais:
-            s.style.flex = `0 0 ${cardWidth}%`;
-            s.style.padding = cardPadding;
+            s.style.flex = `0 0 ${(100 - (numColunas - 1) * gapPercent) / numColunas}%`;
+            s.style.padding = cardPadding; // aplica padding também nos skeletons
             container.appendChild(s);
         }
 
